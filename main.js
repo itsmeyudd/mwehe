ee90eddf7fa6ddc07fd85bed678060430c0df032
ztakeda <yudhasandin@gmail.com>
2023-04-17 04:50:57 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Aku cuma mau bilang", "Selamat Pagiii Cantikkuu", "Aku Sayang Banget Sama Kamu", "I Love You Defi Marta Aurelia"],
+        typeSpeed: 70
+    });
+}
